4cad195fb70a76c95042359c442dfe1bd3095635
heoboseong7 <boseong844@gmail.com>
2020-12-28 09:13:07 +0900

    #4.1 Room Model part One

--- core/models.py
+++ core/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class TimeStampedModel(model.Model):
+class TimeStampedModel(models.Model):
     """ Times Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    # 생성된 날짜 수정된 날짜 자동 업데이트
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     # 데이터베이스에 나타나지 않는 모델 확장을 위해 사용
     class Meta:
