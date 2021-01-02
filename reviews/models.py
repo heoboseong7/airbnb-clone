1de4b287a6337875b10824ef536653cf8e3b4ec8
heoboseong7 <boseong844@gmail.com>
2021-01-02 16:37:09 +0900

    #8.0 ~ #8.1

--- reviews/models.py
+++ reviews/models.py
@@ -3,6 +3,7 @@ from core import models as core_models
 
 
 class Review(core_models.TimeStampedModel):
+
     """ Review Model Definition """
 
     review = models.TextField()
@@ -12,8 +13,27 @@ class Review(core_models.TimeStampedModel):
     location = models.IntegerField()
     check_in = models.IntegerField()
     value = models.IntegerField()
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User", related_name="reviews", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reviews", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.review} - {self.room}"
+
+    # 함수가  admin 패널에만 쓰이는 경우 admin.py에 작성
+    # Front-end, consol, admin에 쓰이는 경우 model.py에 작성
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg, 2)
+
+    rating_average.short_description = "AVG."
