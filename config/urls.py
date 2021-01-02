35b5b57b9938501fe7e4d74443db8099b059350b
heoboseong7 <boseong844@gmail.com>
2021-01-02 17:38:38 +0900

    #8.4 Configuring User Uploads part Two

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+# from . import setting X
+
+# urlpatterns는 항상 유지되어야 한다
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # static url을 데이터가 있는 폴더와 연결
