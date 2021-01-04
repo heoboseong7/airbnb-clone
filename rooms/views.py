82ea933cc1f2765ed0b8450b28f9a0ce1b6d7b18
heoboseong7 <boseong844@gmail.com>
2021-01-04 21:29:06 +0900

    #10.3 Extending Templates part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 from django.shortcuts import render
+from . import models
 
 # django가 발생한 request들을 python object로 변환하여 인자로 넘겨줌
+# html내에서  변수를 사용하는법 : {{var}}
+# logic 사용하는법 ex) for, if :  {%if%} {%else%} {%end if%}  {%for in%} {%end for%}
+# 꼭 end를 사용해야함, indent는 하지 않아도 됨
 def all_rooms(request):
-    return render(request, "all_rooms.html")
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    # context는 html로 변수를 넘겨줌 context={"template에서의 변수명": 값}
