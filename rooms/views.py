10caf799412d14b58d926112db0430d52294a151
heoboseong7 <boseong844@gmail.com>
2021-01-07 14:50:23 +0900

    #11.1 Pagination with Limit and Offset

--- rooms/views.py
+++ rooms/views.py
@@ -7,6 +7,13 @@ from . import models
 # logic 사용하는법 ex) for, if :  {%if%} {%else%} {%end if%}  {%for in%} {%end for%}
 # 꼭 end를 사용해야함, indent는 하지 않아도 됨
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    # page key에 대한 값을 가져옴 없는 경우 기본값 0
+    # url에서 오는 것은 get request
+    all_rooms = models.Room.objects.all()[offset:limit]
+    # [offset:limit]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
     # context는 html로 변수를 넘겨줌 context={"template에서의 변수명": 값}
