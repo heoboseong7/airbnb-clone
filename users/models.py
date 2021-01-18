06b2550a5f2557dc18499a89de05b5ec86ca05ff
heoboseong7 <boseong844@gmail.com>
2021-01-18 18:39:23 +0900

    #16.3 Verifying Email part Two

--- users/models.py
+++ users/models.py
@@ -3,6 +3,10 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+# html태그를 제거한 텍스트로 변환해주는 함수
+from django.utils.html import strip_tags
 
 # Create your models here.
 
@@ -58,11 +62,15 @@ class User(AbstractUser):
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
             send_mail(
                 "Verify Airbnb Account",
-                f"Verify account, this is your code:{secret}",
-                setting.EMAIL_FROM,
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
                 [self.email],
                 fail_silently=False,
+                html_message=html_message,
             )
         return
