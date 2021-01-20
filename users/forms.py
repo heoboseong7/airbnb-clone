44080e05811f3b584623009ffb4dfda6fec831d7
heoboseong7 <boseong844@gmail.com>
2021-01-20 16:31:47 +0900

    #18.3 Closing Thoughts

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -23,29 +24,7 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = models.User
-        fields = ("first_name", "last_name", "email")
-
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+class SignUpForm(UserCreationForm):
     # ModelForm 에서 clean
     # password로 하면 아직 password1이 clean 되기 직전이므로 get password1이 안됨
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
-
-    def save(self, *args, **kwargs):
-        # commit=False인 경우 저장은 하지만 DB에 반영하진 않음
-        user = super().save(commit=False)
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user.username = email
-        user.set_password(password)
-        user.save()
+    username = forms.EmailField(label="Email")
